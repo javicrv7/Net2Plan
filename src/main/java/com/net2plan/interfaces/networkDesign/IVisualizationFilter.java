918491d28eaac01cb97d1c2eb8d536fbcf107315
César San Nicolás <cesarfsannicolasmartinez@gmail.com>
2017-02-15 23:23:04 +0100

    Added IVisualizationFilter code

--- src/main/java/com/net2plan/interfaces/networkDesign/IVisualizationFilter.java
+++ src/main/java/com/net2plan/interfaces/networkDesign/IVisualizationFilter.java
@@ -1,7 +1,25 @@
 package com.net2plan.interfaces.networkDesign;
 
+import com.net2plan.interfaces.networkDesign.Demand;
+import com.net2plan.interfaces.networkDesign.Link;
+import com.net2plan.interfaces.networkDesign.NetworkElement;
+import com.net2plan.internal.IExternal;
+import com.net2plan.utils.Pair;
+
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 /**
- * Created by Cesar on 15/02/2017.
+ * Created by César on 15/11/2016.
  */
-public interface IVisualizationFilter {
+   /*Forwarding Rules are defined in a Map
+    //Key: Pair<Demand,Link>
+    //Value: Double*/
+public interface IVisualizationFilter extends IExternal
+{
+    Map<Class<? extends NetworkElement>, Set<NetworkElement>> executeFilter(NetPlan netPlan, NetworkLayer layer, Map<String, String> filtersParameters, Map<String, String> net2PlanParameters);
+    String getDescription();
+    String getUniqueName();
 }
