b45370332645234af3388726d252e32f6cd3fd42
César San Nicolás <cesarfsannicolasmartinez@gmail.com>
2017-02-15 23:15:17 +0100

    Added new classes

---
+++ src/main/java/com/net2plan/gui/utils/visualizationFilters/VisualizationFiltersController.java
@@ -0,0 +1,7 @@
+package com.net2plan.gui.utils.visualizationFilters;
+
+/**
+ * Created by Cesar on 15/02/2017.
+ */
+public class VisualizationFiltersController {
+}
---
+++ src/main/java/com/net2plan/interfaces/networkDesign/IVisualizationFilter.java
@@ -0,0 +1,7 @@
+package com.net2plan.interfaces.networkDesign;
+
+/**
+ * Created by Cesar on 15/02/2017.
+ */
+public interface IVisualizationFilter {
+}
